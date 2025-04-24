589cfc9df233059f849cfcc26afa384b34c39260
Amrender kumar <amrenderk66@gmail.com>
2025-04-24 21:29:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+cont data = [
+    {
+        "name": "rahul",
+        "age": 23,
+        "club": "Red"
+    }
+    {
+        "name": "vivek",
+        "age": 25,
+        "club": "Blue"
+    }
+    {
+        "name": "denesh",
+        "age": 27,
+        "club": "Sky"
+    }
+];
+
+console.table(data);
